1a50e0a9baf776ea11f0f41686699aac372fd598
liuchuo <95323362@qq.com>
2019-05-13 13:46:00 +0800

    Update ALGO-5 算法训练 最短路.cpp

--- 算法训练/C++/ALGO-5 算法训练 最短路.cpp
+++ 算法训练/C++/ALGO-5 算法训练 最短路.cpp
@@ -1,57 +1,23 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
-#include<queue>
+#include <iostream>
 using namespace std;
-const int maxn = 200010, inf = 99999999;
-struct Edge{
-	int from, to, dist;
-	Edge(int u, int v, int d):from(u), to(v), dist(d) { }
-};
-vector<Edge> edges;
-vector<int> G[maxn];
-int n, m, d[maxn];
-//int cnt[maxn];		//	用于判断是否有负环	 
-bool inq[maxn];		// 判断结点是否在队列中 
-//bool bellman_ford(int s){
-void bellman_ford(int s){
-	queue<int> Q;
-	fill(inq, inq+maxn, false);
-	//memset(cnt, 0, sizeof(cnt));
-	for(int i = 1; i <= n; i++)	d[i] = inf;
-	d[s] = 0;
-	inq[s] = true;
-	Q.push(s);
-	
-	while(!Q.empty()){
-		int u = Q.front(); Q.pop();
-		inq[u] = false;
-		for(int i = 0; i < G[u].size(); i++){
-			Edge& e = edges[G[u][i]];
-			if(d[u] < inf && d[e.to] > d[u] + e.dist){
-				d[e.to] = d[u] + e.dist;
-				if(!inq[e.to]){
-					Q.push(e.to);
-					inq[e.to] = true;
-//					if(++cnt[e.to] > n)
-//						return false;
-				}
-			}
-		}
-	}
-//	return true;
-} 
-int main(){
-	scanf("%d%d", &n, &m);
-	for(int i=0;i<m;i++){
-		int from, to, dist;
-		scanf("%d%d%d", &from, &to, &dist);
-		edges.push_back(Edge(from, to, dist));
-		G[from].push_back(edges.size()-1);
-	}
-	bellman_ford(1);
-	for(int i=2;i<=n;i++){
-		printf("%d\n", d[i]);
-	}
-	return 0;
+int main() {
+    int dis[20001] = {0}, u[200001], v[200001],w[200001];
+    int n, m, inf = 99999999;
+    fill(dis+2,dis+20001,inf);
+    scanf("%d %d", &n, &m);
+    for(int i = 1;  i <= m; i++)
+        scanf("%d %d %d",&u[i], &v[i], &w[i]);
+    for(int k = 1; k <= n - 1; k++){
+        int  check = 0;
+        for(int i = 1; i <= m; i++){
+            if(dis[v[i]] > dis[u[i]] + w[i]){
+                dis[v[i]] = dis[u[i]] + w[i];
+                check = 1;
+            }
+        }
+        if(check == 0) break;
+    }
+    for(int i = 2; i <= n; i++)
+        printf("%d\n",dis[i]);
+    return 0;
 }
