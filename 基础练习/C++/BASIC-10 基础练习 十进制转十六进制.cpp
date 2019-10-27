11e8f4878bb9bf48567ff920c9cf23764525bfc9
byezhao白兆 <52312161+byezhao@users.noreply.github.com>
2019-10-27 11:58:15 +0800

    Update BASIC-10 基础练习 十进制转十六进制.cpp

--- 基础练习/C++/BASIC-10 基础练习 十进制转十六进制.cpp
+++ 基础练习/C++/BASIC-10 基础练习 十进制转十六进制.cpp
@@ -1,23 +1,12 @@
-#include <iostream>
+#include<iostream>
+
 using namespace std;
-int main() {
-    int a;
-    cin >> a;
-    int i = 0;
-    char b[100];
-    if (a == 0) {
-        cout << "0";
-    } else {
-        while (a != 0) {
-            if (a % 16 >= 10)
-                b[i++] = a % 16 + 'A' -10;
-            else
-                b[i++] = a % 16 + '0';
-            a = a / 16;
-        }
-    }
+
+int main()
+{
+    int n;
+    cin >> n;
+    printf("%X", n);
     
-    for (int k = i - 1; k >= 0; k--)
-            cout << b[k];
     return 0;
 }
